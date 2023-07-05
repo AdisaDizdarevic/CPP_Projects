27048a0babe626af44a046449da0873a1ee4da26
Adisa Dizdarevic <adisa.dizdarevic@bicomsystems.com>
2023-07-05 11:40:10 +0200

    Rijesen problem race condition

--- thread.cpp
+++ thread.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
+#include <memory>
+#include <mutex>
+#include <shared_mutex>
 #include <thread>
 
 using namespace std;
+mutex m;
 
 int sharedVariable = 0;
 
 void incrementSharedVariable() {
 
   for (int i = 0; i < 100000; ++i) {
-
+    unique_lock lck{m};
     sharedVariable++;
+
+    lck.unlock();
   }
 }
 
 int main() {
+
   std::thread t1(incrementSharedVariable);
   std::thread t2(incrementSharedVariable);
   std::thread t3(incrementSharedVariable);
